d24e0e3385441bd3fd680c5f997eddf136ee3f86
vkoshk <Vadim.Koshkarev@ciber.no>
2017-12-18 17:15:35 +0100

    task 3.33

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import {ProductService} from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  private albumInfo;
+  albumInfo;
 
   constructor(private _productService: ProductService) { }
 
